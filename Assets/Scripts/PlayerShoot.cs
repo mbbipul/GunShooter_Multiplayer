e24e394d5f5ab9a78c77f5fce60decfd5d0ed1a6
mbbipul <30537029+mbbipul@users.noreply.github.com>
2018-08-26 16:02:15 +0600

    ep 13:Muzzle Fire

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,24 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(WeaponManager))]
 public class PlayerShoot : NetworkBehaviour {
 
     private const string PLAYER_TAG = "Player";
-
-    [SerializeField]
-    private PlayerWeapon weapon;
-
-    [SerializeField]
-    private string weaponLayerName = "Weapon";
-
-    [SerializeField]
-    private GameObject weaponGFX;
 
     [SerializeField]
     private Camera cam;
 
     [SerializeField]
     private LayerMask mask;
+
+    private WeaponManager weaponManager;
+    private PlayerWeapon currentWeapon;
 
     void Start()
     {
@@ -27,28 +22,82 @@ public class PlayerShoot : NetworkBehaviour {
             Debug.LogError("PlayerShoot: No Camera referenced");
             this.enabled = false; 
         }
-
-        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
+        weaponManager = GetComponent<WeaponManager>();
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        { 
-            Shoot();
+        currentWeapon = weaponManager.GetCurrentWeapon();
+
+        if(currentWeapon.fireRate <= 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
         }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                InvokeRepeating("Shoot", 0f, 1f / currentWeapon.fireRate);
+            }
+            else if (Input.GetButtonUp("Fire1"))
+            {
+                CancelInvoke("Shoot");
+            }
+        }
+       
+    }
+
+    //called on the server when a player shoots
+    [Command]
+    void CmdOnShoot()
+    {
+        RpcDoShootEffect();
+    }
+
+    //Is called on all clients when we need to do
+    // a shot effect
+    [ClientRpc]
+    void RpcDoShootEffect()
+    {
+        weaponManager.GetCurrentGraphics().muzzleFlash.Play();
+    }
+
+    [Command]
+    void CmdOnHit (Vector3 _pos, Vector3 _normal)
+    {
+        RpcDoHitEffect(_pos, _normal);
+    }
+
+    [ClientRpc]
+    void RpcDoHitEffect(Vector3 _pos,Vector3 _normal)
+    {
+        GameObject _hitEffect = (GameObject) Instantiate(weaponManager.GetCurrentGraphics().hitEffectPrefab, _pos, Quaternion.LookRotation(_normal));
+        Destroy(_hitEffect, 2f);
     }
 
     [Client]
     void Shoot()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        //we are shooting ,call the onShoot method on the server
+        CmdOnShoot();
+
         RaycastHit _hit;
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit,weapon.range ,mask))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, currentWeapon.range ,mask))
         {
             if(_hit.collider.tag == PLAYER_TAG)
             {
-                CmdPlayerShot(_hit.collider.name, weapon.damage);
+                CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
             }
+
+            //we hit something,call CmdOnHit method on server
+            CmdOnHit(_hit.point, _hit.normal);
         }
     }
 
